f0122c553c2728ed0e957c61832488304c8104ef
edward conte <eac267@cornell.edu>
2024-01-02 15:55:23 -0500

    initialized goStore()

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,33 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+//functions
+
+function goTown() {
+}
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+}
+
+function buyHealth() {
+}
+
+function buyWeapon() {
+}
+
+
+function goCave() {
+  console.log("Going to cave.");
+}   
+
+function fightDragon() {    
+  console.log("Fighting dragon.");
+}
